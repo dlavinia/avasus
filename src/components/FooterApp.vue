f2761ff230a2ca0aa75526f338314753a26b0294
dlavinia <dlavinia2003@gmail.com>
2022-12-27 11:43:47 -0300

    Menu and Footer

---
+++ src/components/FooterApp.vue
@@ -0,0 +1,108 @@
+<template>
+    <footer>
+        <section class="footer-1">
+            <h1>Realização</h1>
+            <div> 
+                <img src="@/assets/logo-lais.svg" alt="Lais">
+                <img src="@/assets/logo-ufrn.svg" alt="UFRN">
+            </div>
+        </section>
+
+        <section class="footer-2">
+            <div class="col15">
+                <img src="@/assets/logo-lais.svg" id="logo-footer" alt="Lais">
+                <p>Laboratório de Inovação <br> Tecnológica em Saúde.</p>
+            </div>
+            <div class="col15">
+                <h1>Links Úteis</h1>
+                <nav>
+                    <ul>
+                        <li><a href="#">Início </a></li>
+                        <li><a href="#">Sobre Nós </a> </li> 
+                        <li><a href="#">Módulos </a></li> 
+                        <li><a href="#">Parceiros </a></li> 
+                        <li><a href="#">Transparência </a></li>   
+                    </ul>  
+                </nav>
+            </div>
+            <div class="col15" id="socialmedia">
+                <h1>Redes sociais</h1>
+                <ul>
+                    <li><a href="#"> <img src="@/assets/icons/facebook.svg" alt="facebook"> </a></li>
+                    <li><a href="#"><img src="@/assets/icons/twitter.svg" alt="twitter"></a> </li> 
+                    <li><a href="#"><img src="@/assets/icons/instagram.svg" alt="instagram"></a></li>  
+                </ul>  
+            </div>
+            
+            
+        </section>
+
+        <section class="copyright">
+           <p> 2022 © LAIS (HUOL). Todos os direitos reservados</p>
+        </section>
+    </footer>
+</template>
+<script>
+export default {
+  name: 'FooterApp'
+}
+</script>
+
+<style scoped>
+    footer, footer a{
+        color: #fff;
+    }
+
+    footer h1{
+        font-weight: 600;
+        font-size: 24px;
+        line-height: 29px;
+        margin-bottom: 1rem;
+    }
+
+    footer section{
+        display: flex;
+        padding: 2rem;
+    }
+    .footer-1{
+        background-color: var(--main-color);
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+    }
+
+    .footer-1 div img{
+        margin: 36px;
+    }
+    .footer-2{
+        background-color: var(--footer-dark);
+        justify-content: center;
+    }
+    .footer-2 ul li{
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 35px;
+    }
+    #logo-footer{
+        width: 120px;
+    }
+
+    #socialmedia ul{
+        display: flex;
+    }
+    #socialmedia ul li{
+        margin: 20px 16px;
+    }
+    .col15{
+        width: 20%;
+    }
+
+    .copyright{
+        background-color: var(--footer-color);
+        justify-content: center;
+        font-weight: 400;
+        font-size: 18px;
+    }
+</style>
+
---
+++ src/components/MenuApp.vue
@@ -0,0 +1,137 @@
+<template>
+  <header>
+     
+    <figure class="logo">
+                <img src="@/assets/logo-menu.svg" alt="logo AVASUS">
+          </figure>
+     <img id="menu-mobile" v-on:click="openMenu" src="https://raw.githubusercontent.com/dlavinia/feliznatal/1730a7fd4a3bb7c0edca1c9e7c0b4b3c5c11419a/src/assets/menu-hamburguer.svg" alt="menu">
+     <div id="menu-overlay"  v-on:click="closeMenu" v-if="menuActive"></div>
+     <nav :class="{open:menuActive}">
+          <figure class="logo-menu-mobile">
+                <img src="@/assets/logo-menu.svg" alt="logo AVASUS">
+          </figure>
+          <ul>
+              <li> <a href="http://localhost:8080/">Início</a></li>
+              <li> <a href="#">Sobre Nós</a></li>
+              <li> <a href="#">Cursos</a></li>
+              <li> <a href="#">Parceiros</a></li>
+              <li> <a href="#">Transparência</a></li>
+              <li> <a href="#">Contato</a></li>
+          </ul>
+            <SearchApp></SearchApp>
+            <button  class="btn btn-second"> Entrar </button>
+            <button  class="btn btn-primary"> Cadastrar </button>
+
+          
+     </nav>
+     
+  </header>
+</template>
+
+<script>
+import SearchApp from "@/components/SearchApp.vue";
+export default {
+  name: 'MenuApp',
+  components: {
+    SearchApp
+  },
+  data(){
+      return{
+          menuActive : false
+      }
+  },
+  methods:{
+      openMenu: function(){
+          this.menuActive = true;
+      },
+      closeMenu: function(){
+          this.menuActive = false;
+      }
+  }
+}
+</script>
+<style scoped>
+  header{
+      background-color: #fff;
+      display: flex;
+      justify-content:center;
+      align-items: center;
+      padding:24px 2rem;
+      width: 100%;
+      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+
+  }
+  nav{
+    display: flex;
+    align-items: center;
+  }
+  ul{
+      display: flex;
+      justify-content: space-between;
+      list-style: none;
+      padding: 0 30px;
+  }
+  a{
+      padding: 8px 16px;
+      font-size: 18px;
+      font-weight: 600;
+      color: var(--text-color-dark);
+  }
+
+  a:active{
+    color: var(--second-color);
+  }
+  #menu-mobile{
+      display: none;
+  }
+  #menu-overlay{
+      position: fixed;
+      left: 0;
+      top: 0;
+      height: 100vh;
+      width: 40%;
+      background-color: #707070;
+      opacity: .6;
+  }
+  .logo-menu-mobile{
+    display: none;
+  }
+  @media only screen and (max-width: 600px) {
+      #menu-mobile{
+      display: block;
+      width: 42px;
+  }
+  header{
+      padding:0 1rem;
+      justify-content: flex-end;
+
+  }
+  .logo img{
+      width: 90%;
+  }
+  .logo-menu-mobile{
+    display: block;
+  }
+  nav{
+      position: fixed;
+      top: 0;
+      right: 0;
+      width: 70%;
+      height: 100vh;
+      padding-top: 20%;
+      background-color:#fff;
+      display: none;
+    
+  }
+  .open{
+      display: block;
+  }
+  ul{
+      flex-direction: column;
+      text-align: left;
+  }
+  li{
+      margin-top: 20px;
+  }
+  }
+</style>
