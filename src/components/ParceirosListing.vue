092ed56fba4dd5e7ef74e32252614c3bd8063112
dlavinia <dlavinia2003@gmail.com>
2022-12-27 18:18:54 -0300

    Pagination and count of Parceiros page

--- src/components/ParceirosListing.vue
+++ src/components/ParceirosListing.vue
@@ -1,44 +1,68 @@
 <template>
-  <section class="parceiros">
+  <section>
+    <p class="count">{{ 6*page }} de {{ totalParceiros.length }} encontrados</p>
 
-      <p> {{ listItems.length }}</p>
-
-      <ParceirosCard  v-for="item in listItems" :key="item.id"
-      :titulo=item.titulo 
-      :capa=item.capa ></ParceirosCard> 
+    <section class="parceiros">
+      <ParceirosCard v-for="item in listItems" :key="item.id" :titulo=item.titulo :capa=item.capa></ParceirosCard>
     </section>
+
+    <section class="pagination-container">
+      <paginate v-model="page" :page-count="totalPages" :page-range="5" :margin-pages="0" :click-handler="clickCallback"
+        :next-text="'Próximo'" :container-class="'pagination'" :page-class="'page-item'">
+      </paginate>
+     </section>
+  </section>
 </template>
 <script>
+import Paginate from 'vuejs-paginate-next';
 import ParceirosCard from "@/components/ParceirosCard.vue";
 export default {
-    name: 'ParceirosListing',
-    components:{
-      ParceirosCard
-    },
-    data() {
-      return {
-        listItems: []
-      }
-    },
-    methods: {
-      async getData() {
-        const res = await fetch("http://localhost:3004/parceiros");
-        const finalRes = await res.json();
-        this.listItems = finalRes;
-        console.log(finalRes)
-      }
-    },
-    mounted() {
-      this.getData()
+  name: 'ParceirosListing',
+  components: {
+    ParceirosCard,
+    paginate: Paginate,
+  },
+  data() {
+    return {
+      listItems: [],
+      page: 1,
+      totalPages: 0,
+      totalParceiros: []
     }
+  },
+  methods: {
+    clickCallback: function (pageNum) {
+      console.log(pageNum);
+      this.getData()
+    },
+    async getData() {
+      const res = await fetch("http://localhost:3004/parceiros?_page=" + this.page + "&_limit=6");
+      const finalRes = await res.json();
+      this.listItems = finalRes;
+      console.log(finalRes)
+    },
+
+    async getAll() {
+      const res = await fetch("http://localhost:3004/parceiros");
+      const finalRes = await res.json();
+      this.totalParceiros = finalRes;
+      this.totalPages = Math.ceil(this.totalParceiros.length / 6);
+      console.log(finalRes)
+    },
+  },
+  mounted() {
+    this.getAll()
+    this.getData()
+  }
 }
 
 </script>
 <style scoped>
-  .parceiros{
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-  }
+@import '../styles/pagination.css';
 
+.parceiros {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
 </style>
