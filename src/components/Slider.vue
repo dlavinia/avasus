6f6c1424a0c7e0cfcd041c50a910753868c679ff
dlavinia <dlavinia2003@gmail.com>
2023-01-06 03:49:57 -0300

    Slider home

---
+++ src/components/Slider.vue
@@ -0,0 +1,58 @@
+<template>
+    <Carousel>
+        <Slide v-for="slide in 3" :key="slide">
+            <div class="carousel__item"><img src="@/assets/img1.png"> </div>
+        </Slide>
+        <template #addons>
+            <Navigation />
+            <Pagination />
+        </template>
+    </Carousel>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
+
+import './vue3-carousel/carousel.css'
+
+export default defineComponent({
+    name: 'BasicSlider',
+    components: {
+        Carousel,
+        Slide,
+        Pagination,
+        Navigation,
+    },
+
+})
+</script>
+
+<style>
+.carousel__item {
+    max-height: 600px;
+    width: 100%;
+    font-size: 20px;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.carousel__slide {
+    padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+    box-sizing: content-box;
+    border-radius: 50%;
+    background: #434343;
+    opacity: 0.2;
+    backdrop-filter: blur(2px);
+    color: #fff;
+    height: 50px;
+    width: 50px;
+}
+
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,18 +1,22 @@
 <template>
+ <BasicSlider></BasicSlider>
      <h1 class="title title-home">Módulos Educacionais </h1>
      <DestaquesListing></DestaquesListing>
     <h1 class="title title-home">Parceiros</h1>
     <ParceirosHome></ParceirosHome>
+   
 </template>
 <script>
 import ParceirosHome from "@/components/ParceirosHome.vue";
 import DestaquesListing from "@/components/listings/DestaquesListing.vue";
+import BasicSlider from "@/components/Slider.vue"
 
 export default {
   name: 'HomePage',
   components:{
     ParceirosHome,
-    DestaquesListing
+    DestaquesListing,
+    BasicSlider
   }
 }
 </script>
